534ef37550bd0a38bba9b30381cbe9b415a0b15a
ShanelDavis <nshanelh@gmail.com>
2020-08-20 01:24:43 -0400

    Google Maps API with ReactJS project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+//bootstrap import here
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MyMapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 33.7333, lng: 84.3831 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: 33.7557, lng: 84.3884 }} />
+      )}
+    </GoogleMap>
+  ))
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="text-center">
+          <h1>GOOGLE MAPS API with REACT JS</h1>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div>Brought to you by ShanelHD</div>
+              <div>
+                <MyMapComponent
+                  isMarkerShown
+                  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAN7mwyR_B3NDDLnuwkLds1RU9IUSBklc8&v=3.exp&libraries=geometry,drawing,places"
+                  loadingElement={<div style={{ height: `100%` }} />}
+                  containerElement={<div style={{ height: `400px` }} />}
+                  mapElement={<div style={{ height: `100%` }} />}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
